fa323eb57c95afe2d9d45c62d6cd71ddc2d478fd
Lambda <joelakaya@googlemail.com>
2026-09-26 23:12:19 +0200

    Delete Demarrer_Football_Tracker_V3.bat

--- Demarrer_Football_Tracker_V3.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-title Football Match Tracker V3
-echo.
-echo Demarrage de Football Match Tracker V3...
-echo Ne fermez PAS cette fenetre pendant l'utilisation.
-echo.
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0serveur_v3.ps1"
-pause
